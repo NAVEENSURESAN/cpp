9957b36d14907b957e0dc4d878e33f67b9234a5c
NAVEENSURESAN <naveensuresan@gmail.com>
2025-10-17 14:55:47 +0530

    Update assignment 14.cpp

--- assignment 14.cpp
+++ assignment 14.cpp
@@ -1,82 +1,61 @@
+
 #include <iostream>
-#include <string>
 using namespace std;
 
 class Student {
+protected:
+    string name;
+    int rollno;
+    string course;
+
 public:
-    string Name;
-    int Rollno;
-    string Course;
-
-    Student(string n, int r, string c) {
-        Name = n;
-        Rollno = r;
-        Course = c;
-    }
-
-    void displayStudentDetails() const {
-        cout << "Name: " << Name << endl;
-        cout << "Roll No: " << Rollno << endl;
-        cout << "Course: " << Course << endl;
+    void setStudent(string n, int r, string c) {
+        name = n;
+        rollno = r;
+        course = c;
     }
 };
+
 class Test : virtual public Student {
-public:
+protected:
     int marks1, marks2, marks3;
 
-    Test(string n, int r, string c, int m1, int m2, int m3)
-        : Student(n, r, c) {
-    marks1 = m1;
+public:
+    void setMarks(int m1, int m2, int m3) {
+        marks1 = m1;
         marks2 = m2;
-      marks3 = m3;
-    }
- void displayTestMarks() const {
-        cout << "Subject 1 Marks: " << marks1 << endl;
-        cout << "Subject 2 Marks: " << marks2 << endl;
-        cout << "Subject 3 Marks: " << marks3 << endl;
+        marks3 = m3;
     }
 };
 
 class GraceMarks : virtual public Student {
+protected:
+    int bonusMark;
+
 public:
-    int BonusMark;
-
-    GraceMarks(string n, int r, string c, int bonus)
-        : Student(n, r, c) {
-        BonusMark = bonus;
+    void setBonus(int b) {
+        bonusMark = b;
     }
-
-    void displayGraceMarks() const {
-        cout << "Bonus Mark: " << BonusMark << endl;
-   }
 };
 
 class Result : public Test, public GraceMarks {
-private:
-   int TotalMarks;
-
 public:
-    Result(string n, int r, string c, int m1, int m2, int m3, int bonus)
-        : Student(n, r, c), Test(n, r, c, m1, m2, m3), GraceMarks(n, r, c, bonus) {
-       TotalMarks = m1 + m2 + m3 + bonus;
-    }
-    void calculateAndDisplayTotal() const {
-        cout << "TOTAL MARKS: " << TotalMarks << endl;
-    }
-
-   void displayAllDetails() const {
-        cout << "Final Result Details" << endl;
-        displayStudentDetails();
-      displayTestMarks();
-        displayGraceMarks();
-       calculateAndDisplayTotal();
+    void displayResult() {
+        int total = marks1 + marks2 + marks3 + bonusMark;
+        cout<<"Name: "<<name<<endl;
+        cout<<"Roll No: "<<rollno<<endl;
+        cout<<"Course: "<<course<<endl;
+        cout<<"Marks: "<<marks1<<", "<<marks2<<", "<<marks3<<endl;
+        cout<<"Bonus Marks: "<<bonusMark<<endl;
+        cout<<"Total Marks: "<<total<<endl;
     }
 };
 
 int main() {
-    Result studentResult("sahal", 101, "CS", 75, 85, 90, 5);
-    
-    studentResult.displayAllDetails();
+    Result r;
+    r.setStudent("Alice", 101, "CSE");
+    r.setMarks(80, 85, 90);
+    r.setBonus(5);
+    r.displayResult();
 
-    return 0;
 }
